0a5287674538dea23ea3f80e63b7eaae7b9896c7
paddy15JnJ <44471124+paddy15JnJ@users.noreply.github.com>
2018-11-13 10:18:05 +0000

    Update admin.py

--- PoolToolWebApp-my_new_branch/poolTool/admin.py
+++ PoolToolWebApp-my_new_branch/poolTool/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from salesforce.testrunner.example.universal_admin import register_omitted_classes
+# from salesforce.testrunner.example.universal_admin import register_omitted_classes
 from . import models
 # some admin classes that you wrote manually yet
 # ...
 # end of file
-register_omitted_classes(models)
+# register_omitted_classes(models)
